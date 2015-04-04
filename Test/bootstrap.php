fef943a732a78a22b842523b71eabed0b8557d8a
Larry Lewis <larry@jenolan.org>
2015-04-05 09:07:33 +1000

    Update travis password

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -9,7 +9,7 @@ function getConnection()
     {
 
         $sql = file_get_contents( dirname( __DIR__ ) . '/Structure/structure.mysql.sql');
-        $dbh = new PDO('mysql:host=localhost;dbname=phpmq_test', 'root', '123qweasd'); // Change if you want
+        $dbh = new PDO('mysql:host=localhost;dbname=phpmq_test', 'travis'); // Change if you want
         $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
         $dbh->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         $dbh->exec( $sql );
